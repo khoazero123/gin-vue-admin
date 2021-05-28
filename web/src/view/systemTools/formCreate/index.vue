0f74e7a6ad318e010cee58b97c9abff7bd27e082
Võ Văn Khoa <khoazero123@gmail.com>
2021-05-28 09:42:07 +0700

    Move form-generator to web

--- web/src/view/systemTools/formCreate/index.vue
+++ web/src/view/systemTools/formCreate/index.vue
@@ -1,17 +1,15 @@
 <template>
   <div style="height:80vh">
-    <iframe width="100%" height="100%" :src="basePath+':8888/form-generator/#/'" frameborder="0"></iframe>
+    <iframe width="100%" height="100%" src="/form-generator/#/" frameborder="0"></iframe>
   </div>
 </template>
 
 <script>
-var path = process.env.VUE_APP_BASE_PATH
+
 export default {
     name:"FormGenerator",
-    data(){
-      return{
-        basePath:path
-      }
+    data() {
+      return {}
     },
 };
 </script>
